04192cbec57c22395dcd743a9e66cc52df159c62
DESKTOP-37RI53T\Nghia <nguyenminhnghia.thd@gmail.com>
2021-05-25 22:41:02 +0700

    Code Xây dựng chắc năng đăng nhập

--- login_m.php
+++ login_m.php
@@ -9,19 +9,19 @@ $error = "Username or Password is invalid";
 else
 {
 // Define $username and $password
-$username=$_POST['username'];
-$password=$_POST['password'];
-require 'connection.php';
-$conn = Connect();
+	$username=$_POST['username'];
+	$password=$_POST['password'];
+	require 'connection.php';
+	$conn = Connect();
 
 // SQL query to fetch information of registerd users and finds user match.
-$query = "SELECT username, password FROM MANAGER WHERE username=? AND password=? LIMIT 1";
+	$query = "SELECT username, password FROM MANAGER WHERE username=? AND password=? LIMIT 1";
 
-$stmt = $conn->prepare($query);
-$stmt -> bind_param("ss", $username, $password);
-$stmt -> execute();
-$stmt -> bind_result($username, $password);
-$stmt -> store_result();
+	$stmt = $conn->prepare($query);
+	$stmt -> bind_param("ss", $username, $password);
+	$stmt -> execute();
+	$stmt -> bind_result($username, $password);
+	$stmt -> store_result();
 
 if ($stmt->fetch())  
 {
